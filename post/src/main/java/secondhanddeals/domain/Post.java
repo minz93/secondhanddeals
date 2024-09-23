e3ae54c59f94f42f824fda88ceb5ea1ea6194c44
minz93 <kimminji7595@gmail.com>
2024-09-24 03:24:57 +0900

    commit

--- post/src/main/java/secondhanddeals/domain/Post.java
+++ post/src/main/java/secondhanddeals/domain/Post.java
@@ -8,7 +8,6 @@ import lombok.Data;
 import secondhanddeals.PostApplication;
 import secondhanddeals.domain.PostDeleted;
 import secondhanddeals.domain.PostEdited;
-import secondhanddeals.domain.PostHided;
 import secondhanddeals.domain.PostWrote;
 import secondhanddeals.domain.StatusUpdated;
 
@@ -41,9 +40,6 @@ public class Post {
     public void onPostPersist() {
         PostWrote postWrote = new PostWrote(this);
         postWrote.publishAfterCommit();
-
-        PostHided postHided = new PostHided(this);
-        postHided.publishAfterCommit();
     }
 
     @PostUpdate
@@ -90,13 +86,6 @@ public class Post {
 
         PostDeleted postDeleted = new PostDeleted(this);
         postDeleted.publishAfterCommit();
-    }
-
-    public void hidePost() {
-        //implement business logic here:
-
-        PostHided postHided = new PostHided(this);
-        postHided.publishAfterCommit();
     }
 
     //<<< Clean Arch / Port Method
