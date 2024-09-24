f42152f84cfb5f5290f3eadaad685c55b38351d5
minji kim <kimminji7595@gmail.com>
2024-09-24 17:27:47 +0000

    d

--- deal/src/main/java/secondhanddeals/domain/Deal.java
+++ deal/src/main/java/secondhanddeals/domain/Deal.java
@@ -41,7 +41,7 @@ public class Deal {
             dealReserved.publishAfterCommit();
         }
     }
-    
+
     @PostUpdate
     public void onPostUpdate() {
         if(this.status.equals("dealCanceled")) {
